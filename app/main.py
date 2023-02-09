bfe3243d86dab393357a12e1dfceb72972ded5bd
garcifer <garcifer@students.zhaw.ch>
2023-02-09 09:06:07 +0100

    engine init order conflict

--- app/main.py
+++ app/main.py
@@ -11,10 +11,10 @@ from fastapi.templating import Jinja2Templates
 
 from sqlalchemy.orm import Session
 
-import helpers
-import crud
-import models
-import my_database
+from app.helpers import *
+from app.crud import *
+from app.models import *
+from app.my_database import engine, SessionLocal, Base
 
 # Create application
 app = FastAPI(title='FastAPI DalLE')
@@ -27,12 +27,12 @@ openai.api_key = os.environ.get("OPENAI_KEY")
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
-models.Base.metadata.create_all(bind=my_database.engine)
+Base.metadata.create_all(bind=engine)
 
 
 # Dependency
 def get_db():
-    db = my_database.SessionLocal()
+    db = SessionLocal()
     try:
         yield db
     finally:
@@ -41,13 +41,13 @@ def get_db():
 
 @app.get("/users/")
 def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
+    users = get_users(db, skip=skip, limit=limit)
     return users
 
 
 @app.get("/images/")
 def get_images(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    images = crud.get_images_from_db(db, skip=skip, limit=limit)
+    images = get_images_from_db(db, skip=skip, limit=limit)
     return images
 
 
@@ -56,12 +56,12 @@ def create_user(username, password, db: Session = Depends(get_db)):
     #db_user = get_user_by_username(db, username=username)
     #if db_user:
     #    raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user_in_db(db, username, password)
+    return create_user_in_db(db, username, password)
 
 
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request, skip: int = 0, limit: int = 5, db: Session = Depends(get_db)):
-    images = crud.get_images_from_db(db, skip=skip, limit=limit)
+    images = get_images_from_db(db, skip=skip, limit=limit)
     print(images)
     # return render_template('index.html', images=images, user_auth=user_auth)
     return templates.TemplateResponse("index.html", {"request": request, "images": images})
@@ -100,7 +100,7 @@ def edit(request: Request):
 def about(request: Request, db: Session = Depends(get_db)):
     # get total images
     # get images in db
-    total_images, num_images = crud.get_db_stats(db)
+    total_images, num_images = get_db_stats(db)
     return templates.TemplateResponse("about.html", {"request": request, "total_images":total_images, "num_images":num_images})
 
 
@@ -129,14 +129,14 @@ async def websocket_endpoint(websocket: WebSocket, db: Session = Depends(get_db)
             else:
                 print(f'CLIENT says - {text}')
                 try:
-                    img_url = helpers.get_dalle_image_url(text)
+                    img_url = get_dalle_image_url(text)
                     #img_url = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-OSSLxbVdVYcONWAYgACXE7BX/user-xe35vqrSPvSsPUEOnVK0Uwov/img-tpCOtVol5LsixwSZRrKvqnYN.png?st=2023-02-08T12%3A23%3A28Z&se=2023-02-08T14%3A23%3A28Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-02-07T21%3A30%3A10Z&ske=2023-02-08T21%3A30%3A10Z&sks=b&skv=2021-08-06&sig=QECDlgMyB6ThTSbTynoDk/uOe2z9866IHV6NlPrWWts%3D"
                     print(img_url)
                 except Exception as e:
                     print(f'Could not get image url for {text}')
                     print(e)
 
-                filename = helpers.make_filename(text)
+                filename = make_filename(text)
                 try:
                     response = requests.get(img_url, stream=True)
                     data = response.content
@@ -144,13 +144,13 @@ async def websocket_endpoint(websocket: WebSocket, db: Session = Depends(get_db)
                     print(f'Could not retrieve image from url: {img_url}')
                     print(e)
                 try:
-                    helpers.create_s3_upload_thread(filename, data)
+                    create_s3_upload_thread(filename, data)
                 except Exception as e:
                     print('Could not save image to S3')
                     print(e)
                 # add file to db
                 try:
-                    helpers.save_image_in_db(db=db, title=text, filename=filename, data=data, url=img_url)
+                    save_image_in_db(db=db, title=text, filename=filename, data=data, url=img_url)
                 except Exception as e:
                     print('Could not save image to DB')
                     print(e)
@@ -178,7 +178,7 @@ async def websocket_moreImages(websocket: WebSocket, db: Session = Depends(get_d
                 break
             else:
                 print(f'CLIENT says - {current_image_offset}')
-                images = crud.get_images_from_db(db, skip=current_image_offset, limit=5)
+                images = get_images_from_db(db, skip=current_image_offset, limit=5)
                 for image in images:
                     json_object = {"title": image.title, "rendered_data":image.rendered_data, "id":image.id}
                     # print(f'Sending {image} to client websocket.')
