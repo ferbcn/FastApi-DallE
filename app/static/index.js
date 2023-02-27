e16a6ea643ad5bf0d0957b38532a5a4c7ef01a65
garcifer <garcifer@students.zhaw.ch>
2023-02-27 17:46:39 +0100

    scroll to top button js/css

--- app/static/index.js
+++ app/static/index.js
@@ -107,3 +107,15 @@ function handleDelete(imageId){
     //xhr.setRequestHeader("accept", "application/json");
     xhr.send("");
 }
+
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
