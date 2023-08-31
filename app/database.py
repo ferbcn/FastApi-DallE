c12307cb8aad1e01c61f5f09a1e376f7fef213d4
ferbcn <17597019+ferbcn@users.noreply.github.com>
2023-08-31 09:49:02 +0200

    Update database.py

--- app/database.py
+++ app/database.py
@@ -9,7 +9,7 @@ DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_URL = os.environ.get('DB_URL')
-DB_PORT = '5432'
+DB_PORT = os.environ.get('DB_PORT')
 
 # ALEMBIC Cheatsheet
 # $ alembic check
