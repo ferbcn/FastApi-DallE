4d0c34b7f7f9bd640dc047c28855e5a052aed5d4
garcifer <garcifer@students.zhaw.ch>
2023-08-31 11:46:00 +0200

    port

--- app/main.py
+++ app/main.py
@@ -236,7 +236,7 @@ if __name__ == '__main__':
     )
     """
     uvicorn.run(
-        'main:app', port=8000, host='127.0.0.1',
+        'main:app', port=8000, host='0.0.0.0',
         reload=True,
     )
 
